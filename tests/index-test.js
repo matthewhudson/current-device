084ffdcec99986fc5d0e0660ef5f24b0fe8fdf8b
matthewhudson <matthud@gmail.com>
2017-12-01 15:20:26 -0500

    Improving test coverage.

--- tests/index-test.js
+++ tests/index-test.js
@@ -5,4 +5,17 @@ describe('current-device', () => {
   it('Exports an `object`', () => {
     expect(device).toBeA('object');
   })
+
+  describe('Exposes attributes for os, type, and orientation', () => {
+    it('Exposes `os` string', () => {
+      expect(device.os).toBeA('string');
+    })
+    it('Exposes `type` string', () => {
+      expect(device.type).toBeA('string');
+    })
+    it('Exposes `orientation` string', () => {
+      expect(device.orientation).toBeA('string');
+    })
+  })
+
 })
