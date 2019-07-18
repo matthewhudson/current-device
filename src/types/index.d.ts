b9ddcd682f45b4a541c786ec9185ebc615100db5
LeeKatz1 <43403037+LeeKatz1@users.noreply.github.com>
2019-07-18 16:00:48 +0300

    Adding TypeScript definition file (#204)

---
+++ src/types/index.d.ts
@@ -0,0 +1,56 @@
+declare module 'current-device' {
+  export interface CurrentDeviceInterface {
+    mobile: () => boolean;
+    tablet: () => boolean;
+    desktop: () => boolean;
+    ios: () => boolean;
+    ipad: () => boolean;
+    iphone: () => boolean;
+    ipod: () => boolean;
+    android: () => boolean;
+    androidPhone: () => boolean;
+    androidTablet: () => boolean;
+    blackberry: () => boolean;
+    blackberryPhone: () => boolean;
+    blackberryTablet: () => boolean;
+    windows: () => boolean;
+    windowsPhone: () => boolean;
+    windowsTablet: () => boolean;
+    fxos: () => boolean;
+    fxosPhone: () => boolean;
+    fxosTablet: () => boolean;
+    meego: () => boolean;
+    television: () => boolean;
+
+    // Orientation
+    landscape: () => boolean;
+    portrait: () => boolean;
+    onChangeOrientation: (newOrientation: DeviceOrientation) => void;
+
+    // Utility
+    noConflict: (currentDevice: CurrentDeviceInterface) => void;
+
+    // Properties
+    type: DeviceType;
+    orientation: DeviceOrientation;
+    os: DeviceOs;
+  }
+
+  export type DeviceType = 'mobile' | 'tablet' | 'desktop' | 'unknown';
+  export type DeviceOrientation = 'landscape' | 'portrait' | 'unknown';
+  export type DeviceOs =
+    | 'ios'
+    | 'iphone'
+    | 'ipad'
+    | 'ipod'
+    | 'android'
+    | 'blackberry'
+    | 'windows'
+    | 'fxos'
+    | 'meego'
+    | 'television'
+    | 'unknown';
+
+  let instance: CurrentDeviceInterface;
+  export default instance;
+}
